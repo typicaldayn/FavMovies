b835fff564b9167b203935a2cac62ca7d0c15208
stasbezhan <krig.ua1968@gmail.com>
2022-08-31 13:52:21 +0300

    Initial Commit

---
+++ FavMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FavMovies
+//
+//  Created by Stas Bezhan on 31.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
